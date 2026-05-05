973db0bccb022eb837e16be66384f4ca2c75d500
Carlos <cordaz@corsusa.com>
2026-05-04 23:04:20 -0500

    feat: env config

--- src/main/java/pe/edu/upc/hiready/HiReadyApplication.java
+++ src/main/java/pe/edu/upc/hiready/HiReadyApplication.java
@@ -1,5 +1,6 @@
 package pe.edu.upc.hiready;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HiReadyApplication {
 
     public static void main(String[] args) {
+        Dotenv.configure().ignoreIfMissing().load()
+                .entries()
+                .forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(HiReadyApplication.class, args);
     }
 
