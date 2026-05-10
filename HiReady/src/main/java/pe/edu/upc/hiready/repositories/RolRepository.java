9d39bc5a81dab4411abde2b3c03cda347d224cf0
wmiguelpaos <118947797+wmiguelpaos@users.noreply.github.com>
2026-05-10 14:32:41 -0500

    Delete Old Code

--- HiReady/src/main/java/pe/edu/upc/hiready/repositories/RolRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.hiready.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.hiready.entities.Rol;
-
-public interface RolRepository extends JpaRepository<Rol, Integer> {
-}
--- HiReady/src/main/java/pe/edu/upc/hiready/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.hiready.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.hiready.entities.User;
-
-public interface UserRepository extends JpaRepository <User, Integer> {
-}
